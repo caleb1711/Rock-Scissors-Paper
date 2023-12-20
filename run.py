2b019cfc75b6786918577aa678d3762b74b46390
caleb1711 <>
2023-12-20 07:36:17 +0000

    set the computers choices

--- run.py
+++ run.py
@@ -3,3 +3,16 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
 
+
+"""set the Game's optoins"""
+
+options = ("rock", "paper", "scissors")
+running = True
+
+
+"""computer chooses randomly between the options"""
+while running:
+    
+        player = None
+        computer = random.choice(options)
+
