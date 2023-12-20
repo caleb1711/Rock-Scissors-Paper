486963db3426190287364e91e50031d35a159289
caleb1711 <>
2023-12-20 07:54:31 +0000

    set an if statement to exit or continue game

--- run.py
+++ run.py
@@ -10,9 +10,50 @@ options = ("rock", "paper", "scissors")
 running = True
 
 
+
 """computer chooses randomly between the options"""
 while running:
     
         player = None
         computer = random.choice(options)
+        score = 0
+        
+        
+        """ Defines player input and checks game status"""
+
+        while player not in options: 
+            player = input("Enter a choice (rock, paper, scissors): ")
+
+        print(f"player: {player}")
+        print(f"computer: {computer}")
+
+        if player == computer:
+            print("It's a tie!")
+
+        elif player == "rock" and computer == "scissors":
+            print("You WIN!")
+            
+        elif player == "paper" and computer == "rock":
+            print("You WIN!")
+
+        elif player == "scissors" and computer == "paper":
+            print("You WIN!")
+            
+
+        else:
+            print("You Lost!")
+
+        """ Options to continue or exit Game""" 
+           
+        if not input("Play again? (y/n): ").lower() == "y":
+            running = False
+
+
+
+
+
+
+
+
+
 
